bc20ab2650ba80cdc875d03485c9e90f0f22c2bf
RalucaM13 <157896258+RalucaM13@users.noreply.github.com>
2024-01-28 19:05:34 +0000

    Updated city and temperature functions

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "b13fe4d2a8e30dd1a60bbd2oaf1tb1be";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function SearchBoxSubmit(event) {
   event.preventDefault();
   let searchBoxInput = document.querySelector("#search-box");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchBoxInput.value;
+
+  searchCity(searchBoxInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", SearchBoxSubmit);
+
+searchCity("Paris");
